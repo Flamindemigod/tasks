900b9ca21e7b857657b8fe2999b9dff2e0c75cca
Savio Andrade <andradesavio9073@gmail.com>
2022-09-05 15:15:02 +0200

    added view switcher

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,28 @@
+import { Box, Button, ButtonGroup } from '@mui/material';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import ListIcon from '@mui/icons-material/List';
 import React from 'react'
 import Footer from './Footer';
 import Header from './Header';
 import AddTask from './task/AddTask';
 import CalenderView from './views/calenderView/CalenderView';
 import ListView from './views/listView/ListView';
+import { useState } from 'react';
 const Main = () => {
+    const [view, setView] = useState(false);
     return (
         <>
             <Header />
             <div className='relative'>
-                <ListView/>
-                <CalenderView />
+                <div className="ml-auto mr-40 w-min">
+                    <ButtonGroup variant="outlined" aria-label="outlined primary button group view-selector">
+                        <Button variant={!view ? "contained" : "outlined"} onClick={()=>{setView(false)}}><ListIcon /></Button>
+                        <Button variant={view ? "contained" : "outlined"} onClick={()=>{setView(true)}}><CalendarTodayIcon /></Button>
+                    </ButtonGroup>
+                </div>
+                <Box sx={{ minHeight: "80vh" }}>
+                    {view ? <CalenderView /> : <ListView />}
+                </Box>
                 <AddTask />
 
             </div>
